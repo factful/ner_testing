7a881942ccbfe2cd7fa3320e3bce8f272316a8b8
Ted Han <ted@knowtheory.net>
2019-03-01 13:29:36 -0800

    Update spacy script to dump json.

---
+++ spacy/spacy.py
@@ -0,0 +1,40 @@
+import json
+import spacy
+from spacy import displacy
+
+import xx_ent_wiki_sm
+nlp = xx_ent_wiki_sm.load()
+
+import glob
+import os
+
+dirname = os.path.dirname(__file__)
+for input_path in glob.glob(os.path.join(dirname, '..', 'documents', '*.txt')):
+  text = open(input_path, 'r').read()
+  doc = nlp(text)
+  #for ent in doc.ents:
+  #    print(ent.text, ent.start_char, ent.end_char, ent.label_)
+  #displacy.serve(doc, style='ent')
+
+  ents = []
+  for ent in doc.ents:
+    entData = { 
+      "name": ent.text, 
+      "type": ent.label_, 
+      "matches": [{
+        "start":ent.start_char, 
+        "end": ent.end_char, 
+        "label":ent.label_}] 
+    }
+    print(entData)
+    ents.append(entData)
+
+  html = displacy.render(doc, style='ent')
+  basename = os.path.basename(input_path)
+  jsonpath = os.path.join(dirname, basename.replace('.txt', '.spacy.json'))
+  with open(jsonpath, 'w') as jsonFile:
+    json.dump({"entities": ents}, jsonFile)
+  outpath = os.path.join(dirname, basename.replace('.txt', '.html'))
+  with open(outpath, 'w') as outFile:
+    outFile.write(html)
+
